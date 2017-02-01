4414bac39567c722db755c2190ff3d8540ed75cd
CapWebSolutions <matt@capwebsolutions.com>
2017-02-01 11:07:18 -0500

    Initial commit of v1

---
+++ soc-simple-google-analytics.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Systems Of Change Simple Google Analytics Plugin
+Plugin URI: http://github.com/capwebsolutions/soc-simple-google-analytics-plugin
+Description: Adds a Google analytics trascking code to the <head> of your theme, by hooking to wp_head.
+Author: Matt Ryan
+Version: 1.0
+ */
+
+function cws_wpmudev_google_analytics() { ?>
+	<script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-91268830-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+<?php }
+
+add_action( 'wp_head', 'cws_wpmudev_google_analytics', 10 );
